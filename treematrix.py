b0725e4dcfa0bd7797ebcbbdd163551b0a64d98c
xiaommms <2253651405@qq.com>
2022-10-27 10:36:25 +0800

    注释

--- treematrix.py
+++ treematrix.py
@@ -1,8 +1,19 @@
+import argparse
+import pandas as pd
+import numpy as np
+
+parser = argparse.ArgumentParser(description='nwk to matrix')
+parser.add_argument("-n","--nwk",)
+parser.add_argument("-f","--file",)
+args = parser.parse_args()
+file = args.nwk
+path= args.file
+
 pointlist = {}
 roaddict = {}
 
 temp_line = ''
-with open('OG0012067.nwk', 'r') as ff :
+with open(file, 'r') as ff :
     for line in ff :
         temp_line = line.strip()
 
@@ -11,43 +22,35 @@ regionstr = ''
 for i in temp_line :
 
     if '(' == i : 
-        deepflag.append('0')
-        #print('(', ''.join(deepflag))
+        deepflag.append('0')   #节点深度
         continue
 
-    if ')' == i : 
-        deepstr = ''.join(deepflag)
-        deepflag.pop()
+    if ')' == i :   #中间节点
+        deepstr = ''.join(deepflag) 
+        deepflag.pop() #深度-1
 
         splitstr = regionstr.split(':')
         if splitstr[0].startswith('X') :
-            pointlist.setdefault(splitstr[0], deepstr)
-            roaddict.setdefault(deepstr, float(splitstr[1]))
-            
-        else :
-            roaddict.setdefault(deepstr, float(splitstr[1]))
+            pointlist.setdefault(splitstr[0], deepstr)     #叶子节点字典，叶子节点名为key,深度路径为value
+        roaddict.setdefault(deepstr, float(splitstr[1]))    #进化树所有节点字典，深度路径为key,支长为value
         regionstr = ''
-        #print(')', deepstr)
         continue
 
-    if ',' != i : regionstr += i; continue
+    if ',' != i : regionstr += i; continue  #节点名获取
 
     splitstr = regionstr.split(':')
     deepstr = ''.join(deepflag)
+
     if splitstr[0].startswith('X') :
         pointlist.setdefault(splitstr[0], deepstr)
         roaddict.setdefault(deepstr, float(splitstr[1]))
-        deepflag[-1] = str(int(deepflag[-1])+1)
+        deepflag[-1] = str(int(deepflag[-1])+1) #深度加1
         deepstr = ''.join(deepflag)
     else :
         roaddict.setdefault(deepstr, float(splitstr[1]))
         deepflag[-1] = str(int(deepflag[-1])+1)
         deepstr = ''.join(deepflag)
     regionstr = ''
-    #print(',', deepstr)
-
-#print(roaddict)
-#print(pointlist)
 
 def calc_sum(t1, t2) :
     if t1 == t2 : return 0
@@ -57,30 +60,25 @@ def calc_sum(t1, t2) :
     for s in range(min(len(code1), len(code2))) : 
         if code1[s] != code2[s] : break
 
-    for i in range(s+1, len(code1)+1) : res += roaddict.get(code1[:i], 0)#; print(code1[:i], roaddict.get(code1[:i], 0))
-    for i in range(s+1, len(code2)+1) : res += roaddict.get(code2[:i], 0)#; print(code2[:i], roaddict.get(code1[:i], 0))
+    for i in range(s+1, len(code1)+1) : res += roaddict.get(code1[:i], 0)
+    for i in range(s+1, len(code2)+1) : res += roaddict.get(code2[:i], 0)
 
-    return res
+    return ("%.2f"%float(res))
 
 res_matrix = {}
+mat1 = np.zeros([len(list(pointlist.keys())),len(list(pointlist.keys()))])
+table = pd.DataFrame(mat1,columns = list(pointlist.keys()),index=list(pointlist.keys()))
 
 for t1 in list(pointlist.keys()) : 
     for t2 in list(pointlist.keys()) :
         tres = calc_sum(t1, t2)
-        #print('{} {} : {}'.format(t1, t2, tres))
         if t1 not in res_matrix : res_matrix.setdefault(t1, {t2: tres}); continue
         if t2 not in res_matrix[t1] : res_matrix[t1].setdefault(t2, tres); continue
 
-print('\n\n\ntab\t'+'\t'.join(list(res_matrix.keys())))
-for k, v in res_matrix.items() :
-    print(k, end='\t')
-    print('\t'.join([str(x) for x in list(v.values())]))
-'''
-test1 = 'XP_008355427.2'
-test2 = 'XP_008386409.1'
-print('\n\n\n', test1, test2, calc_sum(test1, test2))
-
-test1 = 'XP_020867760.1'
-test2 = 'XP_015575846.2'
-print('\n\n\n', test1, test2, calc_sum(test1, test2))
-'''
+key = list(res_matrix.keys())
+for k in range(len(key)):
+    va = list(res_matrix[key[k]].values())
+    for v in range(k,len(va)):
+        table.iloc[k,v] = va[v]
+        table.iloc[v,k] = va[v]
+table.to_csv(path)
